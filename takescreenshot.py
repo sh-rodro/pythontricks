17104c888af1aca0aebfd7f398a61ab631490c5c
Sakhawat Hossain Rodro <73511301+sh-rodro@users.noreply.github.com>
2022-04-28 16:36:50 +0600

    Create takescreenshot.py

---
+++ takescreenshot.py
@@ -0,0 +1,20 @@
+#Python Program to taking a screenshot
+ 
+#To import pyautogui module
+import pyautogui 
+ 
+#To import time module
+import time 
+ 
+#To take screenshot after 5 seconds
+time.sleep(5) 
+ 
+#To take screenshot 
+screenshot = pyautogui.screenshot() 
+ 
+#To save the screenshot
+#This image will be saved inside this directory
+screenshot.save('image.png')  
+ 
+#Will print a message in terminal, after taken the screenshot
+print('screenshot taken.') 
